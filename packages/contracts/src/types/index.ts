ffbdec60c295c5bfedf1b815fee6675d74233e0b
Milan Steiner <steiner.milan@hotmail.com>
2023-12-05 14:06:36 +0100

    Export new bindings properly

--- packages/contracts/src/types/index.ts
+++ packages/contracts/src/types/index.ts
@@ -1,3 +1,5 @@
 export * as Cw20Types from "./Cw20.types";
 export * as WhelpPoolTypes from "./WhelpPool.types";
 export * as WhelpStakeTypes from "./WhelpStake.types";
+export * as WhelpMultiHopTypes from "./WhelpMultiHop.types";
+export * as WhelpFactoryTypes from "./WhelpFactory.types";
